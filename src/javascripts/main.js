0bcc28881304d6cea2c5440276d8df5f08a7500c
Abdulmalek Al-Gahmi <aalgahmi@gmail.com>
2021-10-27 13:14:40 -0600

    Added bootstrap support

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,3 +4,5 @@ require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
 require.context('../stylesheets/', true, /\.(css)$/i)
 
 //TODO - Your ES6 JavaScript code (if any) goes here
+import 'bootstrap'
+
