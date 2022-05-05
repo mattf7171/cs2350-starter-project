2f502819d1d7177ad2eb10f9859a4d727b93956a
Abdulmalek Al-Gahmi <aalgahmi@aalgahmi.com>
2022-05-04 23:09:56 -0600

    Switch to Vite

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const glob = require('glob')
+import { defineConfig } from 'vite'
+
+let input = {}
+
+glob.sync('./src/**/*.html').map(file => {
+  return [file.substring(`.${path.sep}src${path.sep}`.length), file]
+}).forEach( f => {
+    input[f[0]] = f[1]
+})
+
+console.log(input)
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input
+    }
+  },
+  server: {
+    port: 8080,
+    open: true
+  },
+  plugins: []
+})
